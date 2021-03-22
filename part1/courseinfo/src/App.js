d9f3ea2dd2091967b1b76fa32c1140c75a5c6c98
akrab <abdallahakrab98@gmail.com>
2021-03-22 10:07:39 +0200

    Submission: Part 1

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+const Header = ({ course }) => <div>{course}</div>;
+const Content = ({ parts }) => (
+  <div>
+    <Part part={parts[0]} />
+    <Part part={parts[1]} />
+    <Part part={parts[2]} />
+  </div>
+);
+const Part = ({ part }) => (
+  <div>
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  </div>
+);
+const Total = ({ parts }) => (
+  <div>
+    <p>
+      Number of exercises{" "}
+      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+    </p>
+  </div>
+);
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
