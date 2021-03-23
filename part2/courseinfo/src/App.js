fed4b4a21393f60315b33ce044d45162891c9b11
akrab <abdallahakrab98@gmail.com>
2021-03-23 14:39:39 +0200

    2.4

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,4 +1,4 @@
-const Header = ({ text }) => <h1>{text}</h1>;
+const Header = ({ text }) => <h2>{text}</h2>;
 const Course = ({ course }) => (
   <div>
     <Header text={course.name} />
@@ -11,9 +11,6 @@ const Content = ({ parts }) => (
     {parts.map((part) => (
       <Part part={part} />
     ))}
-    {/* <Part part={parts[0]} />
-    <Part part={parts[1]} />
-    <Part part={parts[2]} /> */}
   </div>
 );
 const Part = ({ part }) => (
@@ -35,27 +32,57 @@ const Total = ({ parts }) => (
 );
 
 export const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      {courses.map((course) => (
+        <Course course={course} />
+      ))}
+    </div>
+  );
 };
