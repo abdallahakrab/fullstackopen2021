3169f47def3ed9225a3f6cd106f7e303c4a8d138
akrab <abdallahakrab98@gmail.com>
2021-03-25 14:13:37 +0200

    2.17

--- part2/phonebook/src/Contacts.js
+++ part2/phonebook/src/Contacts.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Contact from "./Contact";
-function Contacts({ contacts }) {
+function Contacts({ contacts, onDelete }) {
   return (
     <div>
       {contacts.map((contact) => (
-        <Contact contact={contact} key={contact.name}>
+        <Contact contact={contact} onClickDelete={onDelete} key={contact.id}>
           {contact.name}
           {contact.number}
         </Contact>
