2e81ae8ad8392d8d5be08da630cac62b186d54c8
abdallah akrab <abdallahakrab98@gmail.com>
2021-04-01 14:27:51 +0200

    resolving conflicts

--- part4/index.js
+++ part4/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const app  = require('./app')
 const config = require('./utils/config');
 const logger = require('./utils/logger')
@@ -12,52 +11,3 @@ app.listen(PORT, () => {
 })
 
 module.exports = app
-=======
-require('dotenv').config();
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-
-const config = require('./utils/config')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
->>>>>>> 4c389874ee469c3513000f14f6a32f402435c327
