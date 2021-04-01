ac8d4a3dfda6398d2428c06527f76f5a2f3964b5
abdallah akrab <abdallahakrab98@gmail.com>
2021-04-01 21:07:09 +0200

    4.3-4.4: list_helper functions & test

---
+++ part4/utils/list_helper.js
@@ -0,0 +1,18 @@
+
+const dummy = (array) =>{
+    return 1
+}
+
+const totalLikes = (array) =>{
+
+    const reducer = (sum,item) =>{
+        return sum += item.likes
+    }
+    return array.reduce(reducer,0)
+}
+
+
+module.exports = {
+    dummy,
+    totalLikes
+}
