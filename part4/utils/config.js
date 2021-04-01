c3904e83fe039910e397ea3f0c96fa5e1d847ada
abdallah akrab <abdallahakrab98@gmail.com>
2021-04-01 14:22:09 +0200

    4.2 Refactor: app, index, logger

--- part4/utils/config.js
+++ part4/utils/config.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 
 const config = {
 PORT: process.env.PORT || 3003,
-MONGODB_URI: "mongodb+srv://akrab:123456a@cluster0.fvbjo.mongodb.net/bloglist?retryWrites=true&w=majority"
+MONGODB_URI: process.env.MONGODB_URI
 
 }
 module.exports = config
