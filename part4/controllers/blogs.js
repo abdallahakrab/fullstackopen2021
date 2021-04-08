b4d0953b7534db9dcff0ed8e53260fdf44740094
abdallah akrab <abdallahakrab98@gmail.com>
2021-04-08 10:39:33 +0200

    4.19: authorize adding blogs

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,23 +1,42 @@
 const Router = require("express").Router;
 const app = require("../app");
 const Blog = require("../models/blog");
+const jwt = require("jsonwebtoken");
+const config = require("../utils/config");
+const User = require("../models/user");
 
 blogsRouter = Router();
+
+const getToken = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user");
   response.json(blogs);
 });
-
+//verify token if not athenic return 401? , verified , extract user id, add new blog to user's bloglist & blog creater
 blogsRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body);
+  const token = getToken(request);
+  const decodedToken = jwt.verify(token, config.SECRET);
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+  const user = User.findById(decodedToken.id);
+  const blog = new Blog({ ...request.body, user: user.id });
   // console.log(request.body);
   if (!blog.url || !blog.title) {
-    return response.status(400).send("error");
+    return response.status(400).json({ error: "blog url or title is missing" });
   }
 
-  const result = await blog.save();
-  response.status(201).json(result);
+  const savedBlog = await blog.save();
+  user.notes = user.notes.concat(savedBlog._id);
+  await user.save();
+  response.status(201).json(savedBlog);
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
