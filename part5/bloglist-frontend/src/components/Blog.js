fb898ad41b9d3ed1b0b0bfda99771cd65c998eca
abdallah akrab <abdallahakrab98@gmail.com>
2021-04-19 12:15:36 +0200

    5.8: Expand Blog info

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const [toggleDetails, setToggleDetails] = useState(false);
+  return (
+    <ul style={blogStyle}>
+      <li>
+        {blog.title}
+        <button
+          onClick={() => {
+            setToggleDetails(!toggleDetails);
+          }}
+        >
+          {toggleDetails ? "hide" : "view"}
+        </button>
+      </li>
+      {toggleDetails ? (
+        <div>
+          <li>{blog.url}</li>{" "}
+          <li>
+            {`likes ${blog.likes}`}
+            <button>like</button>
+          </li>
+          <li>{blog.author}</li>
+        </div>
+      ) : (
+        ""
+      )}
+    </ul>
+  );
+};
+export default Blog;
 
-export default Blog
+const blogStyle = {
+  border: "solid",
+  borderWitdh: 1,
+  borderColor: "black",
+  listStyle: "none",
+};
