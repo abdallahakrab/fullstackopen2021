19cd9ffcc6696d9938afb5f4b292e22a1508a188
Ak <abdallahakrab98@gmail.com>
2021-08-01 11:36:46 +0200

    9.3: bmiCalculator error handling

--- part9/node/bmiCalculator.ts
+++ part9/node/bmiCalculator.ts
@@ -1,16 +1,45 @@
 const calculateBmi = (height: number, weight: number): string => {
-  if (height / weight < 1.85) {
+  const result = (weight / (height * height)) * 1000;
+  if (result < 1.85) {
     return "underweight";
   }
-  if (height / weight < 2.49) {
+  if (result < 2.49) {
     return "healthy";
   }
-  if (height / weight < 2.99) {
+  if (result < 2.99) {
     return "overweight";
   }
-  if (height / weight < 3.99) {
+  if (result < 3.99) {
     return "obese";
   }
   return "undefined range";
 };
-console.log(calculateBmi(180, 74));
+interface InputValues {
+  height: number;
+  weight: number;
+}
+const getInput = (): InputValues => {
+  if (process.argv.length < 4) {
+    throw new Error("few arguments");
+  }
+  if (process.argv.length > 4) {
+    throw new Error("too many arguments");
+  }
+  const height = parseInt(process.argv[2]);
+
+  const weight = parseInt(process.argv[3]);
+  if (isNaN(height) || isNaN(weight)) {
+    throw new Error("only numbers are accepeted");
+  }
+  const inputValues: InputValues = {
+    height: parseInt(process.argv[2]),
+    weight: parseInt(process.argv[3]),
+  };
+  return inputValues;
+};
+try {
+  const values = getInput();
+  console.log(calculateBmi(values.height, values.weight));
+} catch (e) {
+  console.error("error", e.message);
+}
